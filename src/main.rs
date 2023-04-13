31d0ef9e7c7381d782509cba3a5c73cc54926de2
Blakely Haughton <blakely.haughton@gmail.com>
2023-04-13 17:12:36 -0400

    added header analysis code, todo fix decimal zero padding

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,10 @@ struct Column{
     name: String,
     abbv: String,
     unit: String,
-    startbyte: usize,
-    nextbyte: usize,
-    length: usize,
-    data: Vec<i16>
+    //startbyte: usize,
+    //nextbyte: usize,
+    entries: usize,
+    data: Vec<String>
 }
 
 fn main() {
@@ -32,7 +32,7 @@ fn main() {
     let racetypestart = get_usize(&data[36..44]);
     let _unknown = get_i16(&data[44..74]); //bunch of bytes
 
-    let file = RaceData{
+    let mut file = RaceData{
         device: get_utf8(&data[74..82]).to_string(),
         date: get_utf8(&data[92..126]).to_string(),
         time: get_utf8(&data[126..158]).to_string(),
@@ -42,11 +42,10 @@ fn main() {
         racetype: get_utf8(&data[racetypestart .. racetypestart+64]).to_string(),
         columns: Vec::new()
     };
-    dbg!(&headerstart);
 
 /* COLUMN HEADER NOTES
  * 0200 0100 D38702 (variant?)
- * [next entry] [data start] [num data]
+ * [current entry (0)] [next entry] [data start] [num data]
  * 12001 0300 0200 [frequency] 0100 0100 0100
  *
  * [current entry] [next entry] [data start]
@@ -56,7 +55,30 @@ fn main() {
     let trackstart = get_i16(&data[3046..3054]);
 
     // Jump from block to block to count
-    get_i16(&data[headerstart .. headerstart+4]);
+    let mut blockstart = get_usize(&data[headerstart+4 .. headerstart+8]);
+    let mut numblocks = 1;
+    loop {
+        blockstart = get_usize(&data[blockstart+4 .. blockstart+8]);
+
+        let datastart = get_usize(&data[8..12]);
+        let numentries = get_usize(&data[blockstart+12 .. blockstart+20]);
+        let decimalshift = get_usize(&data[blockstart+30 .. blockstart+32]);
+        let thisblock = Column {
+            name: get_utf8(&data[blockstart+32 .. blockstart+64]).to_string(),
+            abbv: get_utf8(&data[blockstart+64 .. blockstart+72]).to_string(),
+            unit: get_utf8(&data[blockstart+72 .. blockstart+80]).to_string(),
+            entries: get_usize(&data[blockstart+12 .. blockstart+20]),
+            data: shift(
+                vec_i16(&data[datastart .. datastart+(numentries*2)]).to_owned(),
+                decimalshift)
+        };
+        file.columns.push(thisblock);
+
+        if blockstart == 0 && numblocks > 0 { break; }
+        numblocks += 1;
+    }
+
+    dbg!(file);
 }
 
 fn vec_i16(data: &[u8]) -> Vec<i16> {
@@ -80,16 +102,26 @@ fn get_usize(data: &[u8]) -> usize {
     //usize::from_le_bytes(data[index..index+8].try_into().unwrap())
 }
 
-fn shift(slice: &[i16], times: usize) -> Vec<String> {
+fn shift(slice: Vec<i16>, times: usize) -> Vec<String> {
     //slice.into_iter()
     //    .map(|x| { *x.to_string(); *x.insert(x.len(),'.') })
     //    .collect::<Vec<&str>>()
+    dbg!(&times);
     let mut out: Vec<String> = Vec::new();
-    for num in slice {
-        let mut val = num.to_string();
-        val.insert(val.len()-times,'.');
-        val.push_str("0");
-        out.push(val);
+    //for num in slice {
+    //    let mut val = num.to_string();
+    //    val.insert(val.len()-times,'.');
+    //    val.push_str("0");
+    //    out.push(val);
+    //}
+    if times < 1 {
+        for num in slice {
+            out.push(num.to_string());
+        }
+    } else {
+        for num in slice {
+            out.push(format!("{:.6}",(num as f32 / (10*times) as f32).to_string()));
+        }
     }
     out
 }
