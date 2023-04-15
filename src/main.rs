e7ac682fa844080fbb51c94b4948989fec26720c
Blakely Haughton <blakely.haughton@gmail.com>
2023-04-15 17:18:08 -0400

    I think I fixed the column parser

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,6 @@ struct Column{
     name: String,
     abbv: String,
     unit: String,
-    //startbyte: usize,
-    //nextbyte: usize,
-    entries: usize,
     data: Vec<String>
 }
 
@@ -26,10 +23,10 @@ fn main() {
         .expect("Failed to read file");
 
     // START OF FILE
-    let headerstart = get_usize(&data[8..12]);
-    let datastart = get_usize(&data[12..20]);
+    let headerstart = get_usize32(&data[8..12]);
+    let datastart = get_usize32(&data[12..20]);
     let _unknown = get_i16(&data[20..36]); //right b4 head
-    let racetypestart = get_usize(&data[36..44]);
+    let racetypestart = get_usize32(&data[36..44]);
     let _unknown = get_i16(&data[44..74]); //bunch of bytes
 
     let mut file = RaceData{
@@ -55,30 +52,34 @@ fn main() {
     let trackstart = get_i16(&data[3046..3054]);
 
     // Jump from block to block to count
-    let mut blockstart = get_usize(&data[headerstart+4 .. headerstart+8]);
-    let mut numblocks = 1;
+    let mut blockstart = headerstart;
+    let mut numblocks = 0;
     loop {
-        blockstart = get_usize(&data[blockstart+4 .. blockstart+8]);
+        // Check for end of header section
+        if blockstart == 0 { break; }
 
-        let datastart = get_usize(&data[8..12]);
-        let numentries = get_usize(&data[blockstart+12 .. blockstart+20]);
-        let decimalshift = get_usize(&data[blockstart+30 .. blockstart+32]);
+        let datastart = get_usize32(&data[blockstart+8 .. blockstart+12]);
+        let numentries = get_usize32(&data[blockstart+12 .. blockstart+16]);
+        let decimalshift = get_usize16(&data[blockstart+30 .. blockstart+34]);
         let thisblock = Column {
             name: get_utf8(&data[blockstart+32 .. blockstart+64]).to_string(),
             abbv: get_utf8(&data[blockstart+64 .. blockstart+72]).to_string(),
             unit: get_utf8(&data[blockstart+72 .. blockstart+80]).to_string(),
-            entries: get_usize(&data[blockstart+12 .. blockstart+20]),
             data: shift(
                 vec_i16(&data[datastart .. datastart+(numentries*2)]).to_owned(),
                 decimalshift)
         };
+
+        // DEBUGGING
+        println!("name: {}, abbv: {}, unit: {}", thisblock.name, thisblock.abbv, thisblock.unit );
+        println!("start: {datastart}\nnumentries: {numentries}\nshift: {decimalshift}");
+        //
+
         file.columns.push(thisblock);
-
-        if blockstart == 0 && numblocks > 0 { break; }
         numblocks += 1;
+        // Get address of next block
+        blockstart = get_usize32(&data[blockstart+4 .. blockstart+8]);
     }
-
-    dbg!(file);
 }
 
 fn vec_i16(data: &[u8]) -> Vec<i16> {
@@ -91,22 +92,28 @@ fn vec_i16(data: &[u8]) -> Vec<i16> {
 fn get_i16(data: &[u8]) -> i16 {
     i16::from_le_bytes([data[0], data[1]])
 }
+fn get_i32(data: &[u8]) -> i32 {
+    i32::from_le_bytes([ data[0], data[1], data[2], data[3] ])
+}
 
 fn get_utf8(slice: &[u8]) -> &str {
     core::str::from_utf8(slice).unwrap()
         .trim_matches(char::from(0))
 }
 
-fn get_usize(data: &[u8]) -> usize {
+fn get_usize16(data: &[u8]) -> usize {
     get_i16(data) as usize
-    //usize::from_le_bytes(data[index..index+8].try_into().unwrap())
+    //usize::from_le_bytes(data.try_into().unwrap())
+}
+fn get_usize32(data: &[u8]) -> usize {
+    get_i32(data) as usize
+    //usize::from_le_bytes(data.try_into().unwrap())
 }
 
 fn shift(slice: Vec<i16>, times: usize) -> Vec<String> {
     //slice.into_iter()
     //    .map(|x| { *x.to_string(); *x.insert(x.len(),'.') })
     //    .collect::<Vec<&str>>()
-    dbg!(&times);
     let mut out: Vec<String> = Vec::new();
     //for num in slice {
     //    let mut val = num.to_string();
